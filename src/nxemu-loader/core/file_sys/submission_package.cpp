b66700429c253b8d5a90fe39f6248a3ee2dd2818
fab918 <56844494+fab918@users.noreply.github.com>
2026-09-20 23:22:40 +0200

    file_sys: Fix multi-program update NCA mapping

--- src/nxemu-loader/core/file_sys/submission_package.cpp
+++ src/nxemu-loader/core/file_sys/submission_package.cpp
@@ -303,8 +303,7 @@ void NSP::ReadNCAs(const std::vector<VirtualFile> & files)
                     // If the last 3 hexadecimal digits of the NCA's TitleID is between 0x1 and
                     // 0x7FF, this is a multi-program update NCA. Otherwise, this is a regular
                     // update NCA.
-                    if ((next_nca->GetTitleId() & 0x7FF) != 0 &&
-                        (next_nca->GetTitleId() & 0x800) == 0)
+                    if ((next_nca->GetTitleId() & 0x7FF) != 0)
                     {
                         ncas[next_nca->GetTitleId()][{cnmt.GetType(), rec.type}] = std::move(next_nca);
                     }
